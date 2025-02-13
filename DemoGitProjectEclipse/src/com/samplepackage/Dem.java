10b3bd87bfc9b4a54877f9599f02467df248eff3
thouxzzh <2k21cse167@gmail.com>
2025-02-13 13:03:38 +0530

    updated dem by dev1

--- DemoGitProjectEclipse/src/com/samplepackage/Dem.java
+++ DemoGitProjectEclipse/src/com/samplepackage/Dem.java
@@ -2,5 +2,7 @@ package com.samplepackage;
 public class Dem {
  public static void main(String args[]) {
 	 System.out.println("Hello World");
+	 System.out.println("Successfully completed");
  }
 }
+  
